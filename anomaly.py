a246dd20355e8497db93f2f2f785f1aba654f120
Vignesh906 <vigneshthati674@gmail.com>
2026-01-16 01:07:42 +0530

    anomaly.py

--- anomaly.py
+++ anomaly.py
@@ -1,6 +1,10 @@
 import pandas as pd
 
 df = pd.read_csv("Aadhaar_Monthly_Enrolment.csv")
+
+df['date'] = pd.to_datetime(df['date'], dayfirst=True, format='mixed')
+
+
 df['total'] = df[['age_0_5','age_5_17','age_18_greater']].sum(axis=1)
 
 def detect_anomaly():
@@ -9,3 +13,4 @@ def detect_anomaly():
 
     df['Anomaly'] = df['total'] > mean + 2*std
     return df[df['Anomaly']==True][['district','date','total']].to_string()
+
